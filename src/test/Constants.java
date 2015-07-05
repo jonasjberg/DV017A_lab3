e70908dfc61bb9a7c33d102bcc7a36db2dfaa410
Jonas Sjöberg <jomeganas@gmail.com>
2015-07-05 18:30:09 +0200

    Importera skelettfiler från föregående lab.

---
+++ src/test/Constants.java
@@ -0,0 +1,16 @@
+/**
+ * DV017A :: Grundläggande programmering i Java
+ * 860224 Jonas Sjöberg
+ * Högskolan i Gävle
+ * tel12jsg@student.hig.se
+ * 
+ * Labb #3    JUnit test suite
+ */
+
+package test;
+
+public class Constants
+{
+    public static final double PRECISION = 0.001;
+    public static final double DELTA     = 1e-15;
+}
