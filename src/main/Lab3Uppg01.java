ff6dc2ebc8219bc83e2bfbf7633013ea6df6de59
Jonas Sjöberg <jomeganas@gmail.com>
2015-07-24 01:58:39 +0200

    Lägg till Uppgift #1.
    Börja på Uppgift #1. Tomt dokument, möjligtvis lämpligt som mall.

---
+++ src/main/Lab3Uppg01.java
@@ -0,0 +1,18 @@
+/**
+ * DV017A :: Grundläggande programmering i Java
+ * 860224 Jonas Sjöberg
+ * Högskolan i Gävle
+ * tel12jsg@student.hig.se
+ *
+ * Labb #3    Uppgift 1
+ */
+
+package main;
+
+public class Lab3Uppg01
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
