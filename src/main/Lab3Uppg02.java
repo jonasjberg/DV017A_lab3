50dfc61c1ad2ff1686316bc9146df35a507e327d
Jonas Sjöberg <jomeganas@gmail.com>
2015-07-24 02:40:03 +0200

    Påbörja Uppgift #2.

---
+++ src/main/Lab3Uppg02.java
@@ -0,0 +1,18 @@
+/**
+ * DV017A :: Grundläggande programmering i Java
+ * 860224 Jonas Sjöberg
+ * Högskolan i Gävle
+ * tel12jsg@student.hig.se
+ *
+ * Labb #3    Uppgift 2
+ */
+
+package main;
+
+public class Lab3Uppg02
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
