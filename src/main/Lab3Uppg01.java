70ae54ecbcbc694cae38cac748276eb74648502a
Jonas Sjöberg <jomeganas@gmail.com>
2015-07-24 02:38:56 +0200

    Uppdatera Uppgift #1.

--- src/main/Lab3Uppg01.java
+++ src/main/Lab3Uppg01.java
@@ -11,8 +11,16 @@ package main;
 
 public class Lab3Uppg01
 {
-    public static void main(String[] args)
-    {
-
-    }
+   /*  // Här refererar (pekar) kalle till ett nytt Person-objekt
+    *  Person kalle = new Person ("Kalle Persson");
+    *  //Hur skriver du här om du vill att referensen kalle *inte* ska peka
+    *    på något objekt ????????
+    *
+    *
+    * SVAR: Om referensen "kalle" inte ska peka på något objekt så måste
+    *       "kalle" sättas till att peka på *ingenting* som kallas 'null'.
+    *       Såhär skriver man för att "kalle" inte ska peka på något objekt:
+    *
+    *           kalle = null;
+    */
 }
