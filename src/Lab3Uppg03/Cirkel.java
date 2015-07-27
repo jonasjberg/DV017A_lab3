69ad934c6fc376766579add25b6589912ad6db5c
Jonas Sjöberg <jomeganas@gmail.com>
2015-07-28 01:19:20 +0200

    Börja på Uppgift #3.

---
+++ src/Lab3Uppg03/Cirkel.java
@@ -0,0 +1,15 @@
+/**
+ * DV017A :: Grundläggande programmering i Java
+ * 860224 Jonas Sjöberg
+ * Högskolan i Gävle
+ * tel12jsg@student.hig.se
+ *
+ * Labb #3    Uppgift 3
+ */
+
+package Lab3Uppg03;
+
+public class Cirkel
+{
+
+}
---
+++ src/Lab3Uppg03/Lab3Uppg03.java
@@ -0,0 +1,17 @@
+/**
+ * DV017A :: Grundläggande programmering i Java
+ * 860224 Jonas Sjöberg
+ * Högskolan i Gävle
+ * tel12jsg@student.hig.se
+ *
+ * Labb #3    Uppgift 3
+ */
+
+package Lab3Uppg03;
+
+public class Lab3Uppg03
+{
+    public static void main(String[] args)
+    {
+    }
+}
