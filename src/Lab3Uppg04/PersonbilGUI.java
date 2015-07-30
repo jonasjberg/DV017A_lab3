05b447b5dde0a544b5b9796f0d08efac7e32b922
Jonas Sjöberg <jomeganas@gmail.com>
2015-07-30 09:29:02 +0200

    Uppdatera PersonbilGUI.

--- src/Lab3Uppg04/PersonbilGUI.java
+++ src/Lab3Uppg04/PersonbilGUI.java
@@ -6,11 +6,24 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class PersonbilGUI extends JFrame
 {
+    
+    Color backgroundColor = Color.RED;
 
     private JPanel contentPane;
+
+    private static Personbil bil1;
+    private static Personbil bil2;
 
     /**
      * Launch the application.
@@ -35,12 +48,81 @@ public class PersonbilGUI extends JFrame
      */
     public PersonbilGUI()
     {
+        initialize();
+        createFrame();
+    }
+    
+    private static void initialize()
+    {
+        bil1 = new Personbil("Saab", 1990, "CCC222", Color.RED);
+        bil2 = new Personbil("Volvo", 1999, "ABC988 ", Color.BLACK);
+    }
+    
+    private void createFrame()
+    {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 450, 300);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(new BorderLayout(0, 0));
         setContentPane(contentPane);
+        GridBagLayout gbl_contentPane = new GridBagLayout();
+        gbl_contentPane.columnWidths = new int[]{0, 0, 0};
+        gbl_contentPane.rowHeights = new int[]{0, 0, 0};
+        gbl_contentPane.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+        gbl_contentPane.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+        contentPane.setLayout(gbl_contentPane);
+        
+        JPanel panel = new JPanel();
+        GridBagConstraints gbc_panel = new GridBagConstraints();
+        gbc_panel.insets = new Insets(0, 0, 5, 5);
+        gbc_panel.fill = GridBagConstraints.BOTH;
+        gbc_panel.gridx = 0;
+        gbc_panel.gridy = 0;
+        contentPane.add(panel, gbc_panel);
+        
+        JLabel lblBilData = new JLabel("Bil #1 Data");
+        panel.add(lblBilData);
+        
+        JPanel panel_2 = new JPanel();
+        panel_2.setBackground(Color.GREEN);
+        GridBagConstraints gbc_panel_2 = new GridBagConstraints();
+        gbc_panel_2.insets = new Insets(0, 0, 5, 0);
+        gbc_panel_2.fill = GridBagConstraints.BOTH;
+        gbc_panel_2.gridx = 1;
+        gbc_panel_2.gridy = 0;
+        contentPane.add(panel_2, gbc_panel_2);
+        
+        JPanel panel_1 = new JPanel();
+        GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+        gbc_panel_1.insets = new Insets(0, 0, 0, 5);
+        gbc_panel_1.fill = GridBagConstraints.BOTH;
+        gbc_panel_1.gridx = 0;
+        gbc_panel_1.gridy = 1;
+        contentPane.add(panel_1, gbc_panel_1);
+        
+        JLabel lblBilData_1 = new JLabel("Bil #2 Data");
+        panel_1.add(lblBilData_1);
+        
+        JPanel panel_3 = new JPanel();
+        panel_3.setBackground(Color.BLACK);
+        GridBagConstraints gbc_panel_3 = new GridBagConstraints();
+        gbc_panel_3.fill = GridBagConstraints.BOTH;
+        gbc_panel_3.gridx = 1;
+        gbc_panel_3.gridy = 1;
+        contentPane.add(panel_3, gbc_panel_3);
     }
-
+    
+    private JLabel createInfoLabel(Personbil bil)
+    {
+    }
+    
+//    private JLabel createInfoLabel(Personbil bil)
+//    {
+//        JLabel label = new JLabel("Bilmodell:           " + bil.getModel()
+//                                + "Årsmodell:           " + bil.getYearManufactured()
+//                                + "Registreringsnummer: " + bil.getRegistrationNumber()
+//                                + "Färg: ");
+//        
+//        return label;
+//    }
 }
