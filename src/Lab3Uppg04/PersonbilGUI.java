d7a48e1e4371b63f2b2f9c858cc007d1db308e1e
Jonas Sjöberg <jomeganas@gmail.com>
2015-07-30 11:42:21 +0200

    Uppdatera PersonbilGUI. Fungerar dugligt.

--- src/Lab3Uppg04/PersonbilGUI.java
+++ src/Lab3Uppg04/PersonbilGUI.java
@@ -50,12 +50,12 @@ public class PersonbilGUI extends JFrame
         createFrame();
     }
 
+    /* Skapa objekt och konfigurera JFrame */
     private void initialize()
     {
         bil1 = new Personbil("Saab", 1990, "CCC222", Color.RED);
-        bil2 = new Personbil("Volvo", 1999, "ABC988 ", Color.BLACK);
+        bil2 = new Personbil("Volvo", 1999, "ABC988 ", Color.BLUE);
 
-        /* Avsluta programmet från GUI:t */
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
@@ -67,25 +67,17 @@ public class PersonbilGUI extends JFrame
         contentPane.setBackground(Color.WHITE);
         setContentPane(contentPane);
         GridBagLayout gbl_contentPane = new GridBagLayout();
-        gbl_contentPane.columnWidths = new int[] { 0, 0, 0 };
+        gbl_contentPane.columnWidths = new int[] { 0 };
         gbl_contentPane.rowHeights = new int[] { 0, 0, 0, 0, 0 };
-        gbl_contentPane.columnWeights = new double[] { 1.0, 1.0,
-                Double.MIN_VALUE };
+        gbl_contentPane.columnWeights = new double[] { 1.0 };
         gbl_contentPane.rowWeights = new double[] { 1.0, 0.0, 1.0, 0.0,
                 Double.MIN_VALUE };
         contentPane.setLayout(gbl_contentPane);
 
-        //      String someLine;
-
-        //      while (whatEver...) {
-        //         ... 
-        //         newLine = String.format(format, aNum, aName, aDate);
-        //         jTextArea1.append(newLine + "\n");
-        //      }
-
         /* Bil #1 "Data" textfält */
         JPanel panelBil1Data = new JPanel();
         panelBil1Data.setBackground(Color.WHITE);
+
         GridBagConstraints gbc_panelBil1Data = new GridBagConstraints();
         gbc_panelBil1Data.insets = new Insets(0, 0, 5, 5);
         gbc_panelBil1Data.fill = GridBagConstraints.BOTH;
@@ -95,30 +87,23 @@ public class PersonbilGUI extends JFrame
 
         JTextArea textBil1 = new JTextArea();
         textBil1.setFont(stdFont);
-
         textBil1.setText("Bil #1 Data");
-        addNewLine(textBil1, "Bilmodell", bil1.getModel());
-        addNewLine(textBil1, "Årsmodell",
+        addLine(textBil1, "Bilmodell", bil1.getModel());
+        addLine(textBil1, "Årsmodell",
                 String.valueOf(bil1.getYearManufactured()));
-        addNewLine(textBil1, "Registreringsnummer",
+        addLine(textBil1, "Registreringsnummer",
                 String.valueOf(bil1.getRegistrationNumber()));
-        addNewLine(textBil1, "Färg", bil1.getColor().toString());
+        addLine(textBil1, "Färg", bil1.getColor().toString());
 
         panelBil1Data.add(textBil1);
 
         /* Bil #1 Färgat område */
+        createCar1ColoredArea();
 
-        /* Bil #1 "Data" textfält */
-        JPanel panelBil1Color = new JPanel();
-        panelBil1Color.setBackground(Color.GREEN);
-        GridBagConstraints gbc_panelBil1Color = new GridBagConstraints();
-        gbc_panelBil1Color.insets = new Insets(0, 0, 5, 5);
-        gbc_panelBil1Color.fill = GridBagConstraints.BOTH;
-        gbc_panelBil1Color.gridx = 0;
-        gbc_panelBil1Color.gridy = 1;
-        contentPane.add(panelBil1Color, gbc_panelBil1Color);
+        /* Bil #2 "Data" textfält */
         JPanel panelBil2Data = new JPanel();
         panelBil2Data.setBackground(Color.WHITE);
+
         GridBagConstraints gbc_panelBil2Data = new GridBagConstraints();
         gbc_panelBil2Data.insets = new Insets(0, 0, 5, 5);
         gbc_panelBil2Data.fill = GridBagConstraints.BOTH;
@@ -129,32 +114,50 @@ public class PersonbilGUI extends JFrame
         JTextArea textBil2 = new JTextArea();
         textBil2.setFont(stdFont);
         textBil2.setText("Bil #2 Data");
-        textBil2.append("\nBilmodell: ");
-        textBil2.append("\nÅrsmodell: ");
-        textBil2.append("\nRegistreringsnummer: ");
-        textBil2.append("\nFärg: ");
+        addLine(textBil2, "Bilmodell", bil2.getModel());
+        addLine(textBil2, "Årsmodell",
+                String.valueOf(bil2.getYearManufactured()));
+        addLine(textBil2, "Registreringsnummer",
+                String.valueOf(bil2.getRegistrationNumber()));
+        addLine(textBil2, "Färg", bil2.getColor().toString());
+
         panelBil2Data.add(textBil2);
 
-        JTextArea txtrTextbilfields = new JTextArea();
-        txtrTextbilfields.setText(
-                "\n" + bil2.getModel() + "\n" + bil2.getYearManufactured()
-                        + "\n" + bil2.getRegistrationNumber());
-        panelBil2Data.add(txtrTextbilfields);
-
         /* Bil #2 Färgat område */
+        createCar2ColoredArea();
+    }
+
+    private void createCar1ColoredArea()
+    {
+        JPanel panelBil1Color = new JPanel();
+        panelBil1Color.setBackground(bil1.getColor());
+
+        GridBagConstraints gbc_panelBil1Color = new GridBagConstraints();
+        gbc_panelBil1Color.insets = new Insets(0, 0, 5, 5);
+        gbc_panelBil1Color.fill = GridBagConstraints.BOTH;
+        gbc_panelBil1Color.gridx = 0;
+        gbc_panelBil1Color.gridy = 1;
+
+        contentPane.add(panelBil1Color, gbc_panelBil1Color);
+    }
+
+    private void createCar2ColoredArea()
+    {
         JPanel panelBil2Color = new JPanel();
-        panelBil2Color.setBackground(Color.BLACK);
+        panelBil2Color.setBackground(bil2.getColor());
+
         GridBagConstraints gbc_panelBil2Color = new GridBagConstraints();
         gbc_panelBil2Color.insets = new Insets(0, 0, 0, 5);
         gbc_panelBil2Color.fill = GridBagConstraints.BOTH;
         gbc_panelBil2Color.gridx = 0;
         gbc_panelBil2Color.gridy = 3;
+
         contentPane.add(panelBil2Color, gbc_panelBil2Color);
     }
 
-    private static void addNewLine(JTextArea textArea, String str1, String str2)
+    private static void addLine(JTextArea textArea, String str1, String str2)
     {
-        final String format = "%1$-25s : %2$15s";
+        final String format = "%1$-20s : %2$-15s";
 
         String line = String.format(format, str1, str2);
         textArea.append("\n" + line);
