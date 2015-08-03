bdc3ae251a51f74ed9c356a0c5f4d251693d77ec
Jonas Sjöberg <jomeganas@gmail.com>
2015-08-03 16:38:23 +0200

    Börja på Uppgift #5.

---
+++ src/Lab3Uppg05/Lab3Uppg05.java
@@ -0,0 +1,20 @@
+/**
+ * DV017A :: Grundläggande programmering i Java
+ * 860224 Jonas Sjöberg
+ * Högskolan i Gävle
+ * tel12jsg@student.hig.se
+ *
+ * Labb #3    Uppgift 5
+ */
+
+package Lab3Uppg05;
+
+import Lab3Uppg04.Personbil;
+
+public class Lab3Uppg05
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
