5402d45e103a2eb9408efba0f6aa5eec51b38a01
Jonas Sjöberg <jomeganas@gmail.com>
2015-08-04 19:14:49 +0200

    Förenkla Uppgift #6.

--- src/Lab3Uppg06/Lab3Uppg06.java
+++ src/Lab3Uppg06/Lab3Uppg06.java
@@ -14,80 +14,61 @@ package Lab3Uppg06;
  */
 public class Lab3Uppg06
 {
-    private static final int[] TESTTAL1 = { 4, 8, 78 };
-    private static final int[] TESTTAL2 = { 100, 23, 27 };
 
     public static void main(String[] args)
     {
-        printSumAndProduct(TESTTAL1);
-        System.out.println("\n");
-        printMinAndMax(TESTTAL2);
-        System.out.println("");
+        printSumAndProduct();
+        printMinAndMax();
     }
 
     /**
-     * Beräknar och skriver ut summan respektive produkten av
-     * talen i arrayen 'numbers' (som måste hålla 3 tal ..)
-     * @param numbers   tal att räkna ut produkt resp. summa för
+     * Beräknar och skriver ut summan respektive produkten av talen. 
      */
-    private static void printSumAndProduct(int[] numbers)
+    private static void printSumAndProduct()
     {
-        if (numbers.length > 3)
-            return;
-
-        int a = numbers[0];
-        int b = numbers[1];
-        int c = numbers[2];
+        int a = 4;
+        int b = 8;
+        int c = 78;
 
         int sum = Berakning.getSum(a, b, c);
         int product = Berakning.getProduct(a, b, c);
 
-        System.out.print("För talen ");
-        printListofNumbers(TESTTAL1);
-        printText("blir summan", sum);
-        printText("och produkten", product);
+        System.out.print("För talen     ");
+        printListofNumbers(a, b, c);
+        System.out.print("\nblir summan   ");
+        printListofNumbers(sum);
+        System.out.print("\noch produkten ");
+        printListofNumbers(product);
+        System.out.println("\n");
     }
-
+    
     /**
-     * Skriver ut det minsta och största talen av talen i arrayen 'numbers' (som
-     * måste hålla 3 tal ..)
-     * @param numbers   tal från vilka det minsta och största skrivs ut
+     * Skriver ut det minsta och största talet.
      */
-    private static void printMinAndMax(int[] numbers)
+    private static void printMinAndMax()
     {
-        if (numbers.length > 3)
-            return;
-
-        int a = numbers[0];
-        int b = numbers[1];
-        int c = numbers[2];
+        int a = 100;
+        int b = 23;
+        int c = 27;
 
         int min = Berakning.getMin(a, b, c);
         int max = Berakning.getMax(a, b, c);
 
-        System.out.print("Bland talen ");
-        printListofNumbers(TESTTAL2);
-        printText("så är det minsta talet", min);
-        printText("och det största talet", max);
+        System.out.print("Bland talen            ");
+        printListofNumbers(a, b, c);
+        System.out.print("\nså är det minsta talet ");
+        printListofNumbers(min);
+        System.out.print("\noch det största talet  ");
+        printListofNumbers(max);
+        System.out.println("\n");
     }
 
     /**
-     * Enkel textformatering skriver ut strängen 'text' följt av siffran
-     * 'number' omgiven av citattecken.
-     * @param text     textdel
-     * @param number     numerisk del
-     */
-    private static void printText(String text, int number)
-    {
-        System.out.print(" " + text + " \"" + number + "\"");
-    }
-
-    /**
-     * Skriver ut tal från en array i en lista. Talen är kommaseparerade med
+     * Skriver ut ett godtyckligt antal tal, kommaseparerade med
      * undantag för det sista talet där ett "och" skrivs ut.
      * @param numbers   tal att skriva ut
      */
-    private static void printListofNumbers(int[] numbers)
+    private static void printListofNumbers(int... numbers)
     {
         for (int i = 0; i < numbers.length; i++) {
             System.out.print("\"" + numbers[i] + "\"");
