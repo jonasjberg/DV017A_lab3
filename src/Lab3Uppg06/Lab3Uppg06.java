5ead8919ef2360b599a5d31758dcd1a5e5dc4d69
Jonas Sjöberg <jomeganas@gmail.com>
2015-08-04 18:02:47 +0200

    Uppdatera testkod för Uppgift #6.

--- src/Lab3Uppg06/Lab3Uppg06.java
+++ src/Lab3Uppg06/Lab3Uppg06.java
@@ -11,8 +11,23 @@ package Lab3Uppg06;
 
 public class Lab3Uppg06
 {
+    private static final int INT_1 = 4;
+    private static final int INT_2 = 8;
+    private static final int INT_3 = 78;
+
     public static void main(String[] args)
     {
 
+        printText("Summan");
+        System.out.println(Berakning.getSum(INT_1, INT_2, INT_3));
+
+    }
+    
+    private static void printText(String s)
+    {
+        System.out.println("");
+        System.out.println(s + " av talen \"" + INT_1 
+                + "\", \"" + INT_2
+                + "\" och \"" + INT_3 + "\":");
     }
 }
