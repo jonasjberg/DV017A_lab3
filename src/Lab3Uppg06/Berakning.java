65a8d3ea035284cef5efa723e25173d451d0662e
Jonas Sjöberg <jomeganas@gmail.com>
2015-08-04 17:38:08 +0200

    Börja på Uppgift #6.

---
+++ src/Lab3Uppg06/Berakning.java
@@ -0,0 +1,15 @@
+/**
+ * DV017A :: Grundläggande programmering i Java
+ * 860224 Jonas Sjöberg
+ * Högskolan i Gävle
+ * tel12jsg@student.hig.se
+ *
+ * Labb #3    Uppgift 6
+ */
+
+package Lab3Uppg06;
+
+public class Berakning
+{
+
+}
